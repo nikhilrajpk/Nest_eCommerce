cead3631c115285b4ece063cd8d8e380d32dd5bc
nikhilrajpk <nikhilrajpk2002@gmail.com>
2024-10-08 14:05:27 +0530

    user registration validation

---
+++ aura_project/authentication_app/validators.py
@@ -0,0 +1,31 @@
+import re
+from django.contrib.auth.password_validation import validate_password
+from django.core.exceptions import ValidationError
+
+
+class Authentication_check:
+    def email_validator(self,email):
+        if not re.match(r'^[a-zA-Z0-9._-]+@[a-zA-Z0-9]+\.[a-zA-Z]{2,}(?:\.[a-zA-Z]{2,})?$', email):
+            return 'Email should be valid!'
+        return None
+            
+    def first_name_validator(self,first_name):
+        if any(char.isdigit() or char.isspace() for char in first_name):
+            return 'First name should not contain number or spaces!'
+        return None
+        
+    def last_name_validator(self,last_name):
+        if any(char.isdigit() or char.isspace() for char in last_name):
+            return 'Last name should not contain number or spaces!'
+        return None
+    
+    def pass_validator(self,password1):
+        try:
+            validate_password(password1)
+        except ValidationError as e:
+            return e.messages
+        return None
+    def password_mismatch(self,password1, password2):
+        if password1 != password2:
+            return 'Passwords do not match'
+        return None
--- aura_project/authentication_app/views.py
+++ aura_project/authentication_app/views.py
@@ -4,21 +4,56 @@ from .forms import UserRegisterForm
 from django.contrib import messages
 from django.contrib.auth import login,authenticate,logout
 from .models import CustomUser
+from .validators import Authentication_check
+
 User = get_user_model()
 
 
 def register_view(request):
+    errors = {}
     if request.method == 'POST':
         form = UserRegisterForm(request.POST or None)
-        print('inside')
         if form.is_valid():
             new_user_exist = CustomUser.objects.filter(email = form.cleaned_data.get('email')).exists()
-            print(new_user_exist)
             if not new_user_exist :
-                new_user = form.save()
+                #created object for user validation Authentication_check
+                user_validation = Authentication_check()
+                
+                email = form.cleaned_data.get('email')
                 first_name = form.cleaned_data.get('first_name')
-                messages.success(request, f'Hey {first_name}, Your account has been created.')
-                return redirect(login_view)
+                last_name = form.cleaned_data.get('last_name')
+                password1 = form.cleaned_data.get('password1')
+                password2 = form.cleaned_data.get('password2')
+                
+                #email validation
+                email_valid = user_validation.email_validator(email)
+                if email_valid:
+                    errors['email'] = email_valid
+                    
+                #first_name validation
+                first_name_valid = user_validation.first_name_validator(first_name)
+                if first_name_valid:
+                    errors['first_name'] = first_name_valid
+                
+                #last_name validation
+                last_name_valid = user_validation.last_name_validator(last_name)
+                if last_name_valid:
+                    errors['last_name'] = last_name_valid
+                    
+                #password validation
+                password_valid = user_validation.pass_validator(password1)
+                if password_valid:
+                    errors['password'] = password_valid
+                
+                #password mismatch checking
+                password_mismatch = user_validation.password_mismatch(password1, password2)
+                if password_mismatch:
+                    errors['password_mismatch'] = password_mismatch
+                
+                if not errors:
+                    new_user = form.save()
+                    messages.success(request, f'Hey {first_name}, Your account has been created.')
+                    return redirect('login')
             else:
                 messages.error(request,'email already exists!')
         else:
@@ -26,7 +61,7 @@ def register_view(request):
     else:
         form = UserRegisterForm()
     context = {
-        'form' : form
+        'form' : form, 'errors': errors
     }
     return render(request,'authentication_app/sign_up.html',context)
 
