c4870b9cd0871e498136c5612ac219b3538bcaa8
nikhilrajpk <nikhilrajpk2002@gmail.com>
2024-10-22 22:25:21 +0530

    coupon code added and discount is calculated

--- aura_project/cart_app/urls.py
+++ aura_project/cart_app/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('cart/',views.cart_view,name='cart'),
     path('cart/update/<int:product_id>/<str:action>/', views.update_cart_item_quantity, name='update_cart_item_quantity'),
     path('remove_cart_item/<int:cart_id>/',views.remove_cart_item,name='remove_cart_item'),
+    path('check_coupon/',views.check_coupon,name='check_coupon'),
+    path('checkout/<int:cart_id>/',views.checkout,name='checkout'),
 ]
--- aura_project/cart_app/views.py
+++ aura_project/cart_app/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render,redirect,get_object_or_404
 from .models import *
 from product_app.models import *
+from coupen_app.models import *
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from decimal import Decimal
 
 # Create your views here.
 
@@ -49,11 +52,47 @@ def cart_view(request):
     # Calculate total cart value
     cart_total = sum(item.total_price for item in cart_items_with_prices)
     
+    cart_total_with_discount = request.session.get('cart_total', None)
+    
+    # Checking if the cart_total_with discount have any value or not
+    if cart_total_with_discount is None:
+        cart_total_with_discount = cart_total
+        request.session['cart_total'] = float(cart_total)
+    
     return render(request,'cart_app/cart.html',{
         'cart': cart,
         'cart_items': cart_items,
-        'cart_total':cart_total,
+        'cart_total':cart_total_with_discount,
     })
+
+def check_coupon(request):
+    cart_total_discount = Decimal(request.session.get('cart_total',0))
+    print(cart_total_discount)
+    if request.method == 'POST':
+        coupon_code = request.POST.get('coupon_code')
+        print(coupon_code)
+        
+        if coupon_code:
+            coupon = get_object_or_404(Coupons, code = coupon_code)
+            print(coupon)
+            
+            if coupon.used_limit > 0:
+                print(coupon.used_limit)
+                cart_total_discount -= coupon.discount_amount
+                
+                print(cart_total_discount)
+                
+                request.session['cart_total'] = float(cart_total_discount)
+                
+                messages.success(request,f'{coupon.code} applied successfully')
+                return redirect('cart_app:cart')
+            else:
+                messages.error(request,f'{coupon.code} is no longer available or expired')
+                return redirect('cart_app:cart')
+    messages.error(request, 'Invalid coupon code.')
+    return redirect('cart_app:cart')
+
+
     
 
 @login_required
@@ -87,3 +126,33 @@ def remove_cart_item(request,cart_id):
     cart_item.delete()
     
     return redirect('cart_app:cart')
+
+
+def checkout(request,cart_id):
+    user_email = request.user.email
+    user = CustomUser.objects.get(email = user_email)
+    cart = Cart.objects.get(id = cart_id)
+    cart_items = cart.items.all()
+    
+    cart_items_with_prices = []
+    # Calculate the total price for each item
+    for item in cart_items:
+        if item.product.offer:
+            item.total_price = item.product.discount_price * item.quantity
+        else:
+            item.total_price = item.product.price * item.quantity
+    
+        cart_items_with_prices.append(item)
+    
+    # Calculate total cart value
+    cart_total = sum(item.total_price for item in cart_items_with_prices)
+    
+    
+    context = {
+        'user':user,
+        'cart_items':cart_items,
+        'cart_total':cart_total,
+    }
+    return render(request,'cart_app/checkout.html',context)
+
+# {% url 'checkout_app:process_order' %}
