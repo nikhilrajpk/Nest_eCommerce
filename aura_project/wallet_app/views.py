4a0539aee67e2612cfb3411a504da2b81ff486ad
nikhilrajpk <nikhilrajpk2002@gmail.com>
2024-11-01 08:42:23 +0530

    wallet details done in user side and walletItems table modified with related name

--- aura_project/wallet_app/views.py
+++ aura_project/wallet_app/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from wallet_app.models import *
 # Create your views here.
+
+def wallet(request):
+    if request.user.is_authenticated and request.user.is_staff:
+        return redirect('admin_app:admin_home')
+    
+    wallet = Wallet.objects.get(user = request.user)
+    wallet_transactions = wallet.transactions.all().order_by('-id')
+    
+    context = {
+        'wallet':wallet,
+        'wallet_transactions':wallet_transactions,
+    }
+    return render(request,'wallet_app/wallet.html',context)
